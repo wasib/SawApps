37e806b244b556a2d176cd087c67fbbf77455790
wasib <mohammedwasib@gmail.com>
2018-08-25 17:22:52 +0530

    section1

---
+++ src/components/Section1.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Section1.css";
+import leftCircle from "../images/Section1/left circle.svg";
+import rightCircle from "../images/Section1/right circle.svg";
+
+export class Section1 extends React.Component {
+  render() {
+    return <div className="background-gradient" />;
+  }
+}
