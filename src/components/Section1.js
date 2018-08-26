fdc78c7917475d00f0d6e9d502286142def0367a
wasib <mohammedwasib@gmail.com>
2018-08-27 02:21:46 +0530

    bg

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "../styles/Section1.css";
-import leftCircle from "../images/Section1/left circle.svg";
-import rightCircle from "../images/Section1/right circle.svg";
 
 export class Section1 extends React.Component {
   render() {
